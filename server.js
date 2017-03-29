aaecb1b4621ac458387be882bb929afd5d19129a
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 13:25:35 +0200

    update 2.0

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require ('express'); 
 var app = express ();
 
-app.get ('/');
+app.get ('/', function (req, res { 
+	res.send ('hello world ') });
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 
 
 console.log ('server ready');
