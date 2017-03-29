a398a82005aa549097234131c24b6e3ad2ea8777
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 14:56:05 +0200

    update 2.1

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ client.on ('ready', () => {
 });
 
 client.on ('message', message => {
+	if (message.author.bot) return;	
 	
-	if (!message.author.bot || message.channel.type == 'dm') 
+	if (message.channel.type == 'dm') 
 	{
 		if (message.content == '!blague') 
 		{
@@ -23,15 +24,29 @@ client.on ('message', message => {
 		else message.reply ("Je n'ai pas compris votre demande"); 
 		console.log (message);
 	}	
-	
+	if (message.mentions.users.get (client.user.id)
+	{
+		var contenu = message.content.replace('<@'+client.user.id+'>', '');		
+
+		if (contenu == '!blague') 
+		{
+			getBlague (function (t)
+			{
+				message.reply(t); 
+			});
+			
+		}
+
+		else message.reply ("Je n'ai pas compris votre demande"); 
+		console.log (message);
+	}	
 	
 });
 
 client.on ('presenceUdapte', function (oldMember, newMember) {
-	console.log ("poerfpsdfkpsdof");
 	console.log (oldMember.presence, '=>', newMember.presence);
-	console.log (newMember.user.username);
-	if (newMember.user.username ==  'bramas' && newMember.presence == true) 
+	console.log ("User : ",newMember.user.username; "Logged in/out");
+	if (newMember.user.username ==  "bramas" && newMember.presence.status == "online") 
 	{
 		newMember.sendMessage ("Bonjour je suis le bot de SU Yiu Quan, que puis-je faire pour vous ?");
 	}
