5008386c15d11b8c28d38411becac0692f1ffae1
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 11:39:18 +0200

    update 1.7

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const Discord = require ('discord.js'); 
+
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = 'Mjk2NTM3ODMyNDMwNzY0MDQ0.C70BqQ.VhGXKjt5as62Up0OQtEwBZl0r5w';
 
-
-
-client.on ('ready', () => {
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
 });
 
-client.on ('message', message => {
-	
-	if (!message.author.bot || message.channel.type == 'dm') 
-	{
-		if (message.content == 'ping') 
-		{
-			message.reply ('pong'); 
-		}
-		else message.reply ('What ?') ;
-		console.log (message);
-	}	
-	
-	
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-client.on ('presenceUdapte', function (oldMember, newMember) {
-	console.log (oldMember.presence, '=>', newMember.presence);
-});
-
-client.login(token); 
-
-
+client.login('Mjk2NTM3ODMyNDMwNzY0MDQ0.C70BqQ.VhGXKjt5as62Up0OQtEwBZl0r5w');
