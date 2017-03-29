29fee37a7f9cb2760dba399900263d145f4f3ca5
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 12:44:17 +0200

    update 2.0

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on ('message', message => {
 });
 
 client.on ('presenceUdapte', function (oldMember, newMember) {
-	if (newMember.displayname == 'ProgSK77') newMember.sendMessage ("Bonjour je suis le bot de SU Yiu Quan, que puis-je faire pour vous ?");
+	if (newMember.displayname == 'ProgSK77' || newMember.nickname == 'ProgSK77') newMember.sendMessage ("Bonjour je suis le bot de SU Yiu Quan, que puis-je faire pour vous ?");
 });
 
 client.login(token); 
