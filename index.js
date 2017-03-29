695c3c251689c8bf12d7db56f10bf708069a8eec
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 11:16:52 +0200

    update 1.5

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const Discord = require ('discord.js'); 
 const client = new Discord.Client();
 const token = 'Mjk2NTM3ODMyNDMwNzY0MDQ0.C70BqQ.VhGXKjt5as62Up0OQtEwBZl0r5w'
+
+
+
 client.on ('ready', () => {
 });
 
 client.on ('message', message => {
-	if (message.content == 'ping') {
-		message.reply ('pong'); 
-	}
+	
+	if (!message.author.bot || message.channel.type == 'dm') 
+	{
+		if (message.content == 'ping') 
+		{
+			message.reply ('pong'); 
+		}
 
-	console.log (message);
+		console.log (message);
+	}	
+	
+	
 });
 
 client.on ('presenceUdapte', function (oldMember, newMember) {
