f064a5fe6be34c7f360ff0227be3eba782f3dce8
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 12:08:42 +0200

    update 1.7

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require ('discord.js');
 const client = new Discord.Client();
 const token = 'Mjk2NTM3ODMyNDMwNzY0MDQ0.C70JUw.bV9_1cq1hsnxWdYdGoGpvEDnVnw';
 
-var serveur = require ('server.js'); 
 
 client.on ('ready', () => {
 });
@@ -26,5 +25,17 @@ client.on ('presenceUdapte', function (oldMember, newMember) {
 });
 
 client.login(token); 
+
+
+var express = require ('express'); 
+var app = express ();
+
+app.get ('/', function (req, res) { 
+	res.send ('hello world');
+
+});
+
+app.listen(process.env.PORT || 5000);
+
 
 
