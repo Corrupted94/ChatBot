9bb7b7568a8ac829654d9a4c435fe9f8b9fd85cf
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 12:11:48 +0200

    update 1.7

--- index.js
+++ index.js
@@ -30,12 +30,9 @@ client.login(token);
 var express = require ('express'); 
 var app = express ();
 
-app.get ('/', function (req, res) { 
-	res.send ('hello world');
+app.get ('/');
 
-});
-
-app.listen(process.env.PORT || 5000);
+app.listen(5000);
 
 
 
