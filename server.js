71cbb74553984d33f00fbdfa53a9d225ef707196
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 13:34:03 +0200

    update 2.0

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require ('express'); 
 var app = express ();
 
-app.get ('/', function (req, res { 
-	res.send ('hello world '); });
+app.get ('/', function (req, res) { 
+	res.send ('hello world '); 
+});
 
 app.listen(process.env.PORT || 5000);
-
 
 console.log ('server ready');
