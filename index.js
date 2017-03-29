98f6090a8ba6397307aec187937f4fcf83603a0a
SU <3603243@ppti-14-503-09.ufr-info-p6.jussieu.fr>
2017-03-29 10:08:39 +0200

    update 1.1

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require ('discord.js'); 
+const client = new Discord.Client();
+
+client.on ('message', message => {
+	if (message.content == 'ping') {
+		message.reply ('pong'); 
+	}
+
+	console.log (message);
+});
+
+client.on ('presenceUdapte', function (oldMember, newMember) {
+	console.log (oldMember.presence, '=>', newMember.presence);
+});
+
+client.login(process.env.DISCORD_TOKEN); 
+
+
