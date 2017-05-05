cec9ee9506b02e5b7b6a8c04491cc610258a68be
SU <3603243@ppti-14-503-16.ufr-info-p6.jussieu.fr>
2017-05-05 19:40:14 +0200

    update 3

--- blague.js
+++ blague.js
@@ -1,8 +1,10 @@
 const axios = require ('axios');
 
 
-module.exports = function (callback) 
-{
-	axios.get ("https://www.chucknorrisfacts.fr/api/get?tri:alea;nb=1").then (function (rep){ callback (rep.data[0].fact); }).catch (console.error);
+module.exports = {
+	
+	getBlague : function (callback) {
+		axios.get("https://www.chucknorrisfacts.fr/api/get?data=tri:alea;nb:1").then (function (rep){ callback(rep.data[0].fact); }).catch (console.error);
 
+	}
 };
