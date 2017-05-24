809f83989e9a687f0bad977454afddd6d0bf9258
Corrupted94 <albert_su@outlook.com>
2017-05-24 03:52:27 +0200

    update 6.1

--- meteo.js
+++ meteo.js
@@ -65,10 +65,10 @@ module.exports = {
 		if (citySplit.length ==1) // s'il n'y a qu'un élément donc on déduit que c'est le nom de la ville.
 		{
 
-			console.log(citySplit[1]);
+			console.log(citySplit[0]);
 			axios.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + citySplit[0] +"&APPID="+ openweatherKEY + "&units=metric" ).then (function (rep){ 
 					
-					callback ("Température aujourd'hui à " + rep.data.city.name + " : " + rep.data.list[0].temp.day + "°C | " + rep.data.list[0].main.weather + ", " + rep.data.list[0].main.description + " et demain : " + rep.data.list[1].temp.day + " °C | " + rep.data.list[1].main.weather + ", " + rep.data.list[1].main.description); 
+					callback ("Température aujourd'hui à " + rep.data.city.name + " : " + rep.data.list[0].temp.day + "°C | " + rep.data.list[0].weather[0].main + ", " + rep.data.list[0].weather[0].description + " et demain : " + rep.data.list[1].temp.day + " °C | " + rep.data.list[1].weather[0].main + ", " + rep.data.list[1].weather[0].description); 
 			
 		
 
@@ -92,7 +92,7 @@ module.exports = {
 					
 						axios.get("http://api.openweathermap.org/data/2.5/forecast/daily?zip=" + citySplit[1] +"&APPID="+openweatherKEY + "&units=metric" ).then (function (rep){ 
 					
-					callback ("Température aujourd'hui à " + rep.data.city.name + " : " + rep.data.list[0].temp.day + "°C | " + rep.data.list[0].main.weather + ", " + rep.data.list[0].main.description + " et demain : " + rep.data.list[1].temp.day + " °C | " + rep.data.list[1].main.weather + ", " + rep.data.list[1].main.description); } ).catch (console.error);
+					callback ("Température aujourd'hui à " + rep.data.city.name + " : " + rep.data.list[0].temp.day + "°C | " + rep.data.list[0].weather[0].main + ", " + rep.data.list[0]..weather[0].description + " et demain : " + rep.data.list[1].temp.day + " °C | " + rep.data.list[1].weather[0].main + ", " + rep.data.list[1].weather[0].description); } ).catch (console.error);
 				
 
 					}
@@ -112,7 +112,7 @@ module.exports = {
 					
 							axios.get("http://api.openweathermap.org/data/2.5/forecast/daily?zip="+ citySplit[1] +"&APPID="+openweatherKEY + "&units=metric" ).then (function (rep){ 
 					
-							callback ("Température dans " + parseInt (citySplit[3])  + " jour(s) à " + rep.data.city.name + " : " + rep.data.list[parseInt (citySplit[3])].temp.day + "°C | " + rep.data.list[parseInt (citySplit[3])].main.weather + ", " + rep.data.list[0].main.description ); } ).catch (console.error);
+							callback ("Température dans " + parseInt (citySplit[3])  + " jour(s) à " + rep.data.city.name + " : " + rep.data.list[parseInt (citySplit[3])].temp.day + "°C | " + rep.data.list[parseInt (citySplit[3])].weather[0].main + ", " + rep.data.list[0].weather[0].description ); } ).catch (console.error);
 				
 
 						}
